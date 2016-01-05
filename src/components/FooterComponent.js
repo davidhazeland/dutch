1d137ce673a0e746e553d6bbd9eb5c55eeec42c7
thanhcong <thanhcong.mssl@gmail.com>
2016-01-05 13:27:03 +0700

    Add footer component

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+
+require('styles/components//Footer.less');
+
+class FooterComponent extends React.Component {
+  render() {
+    return (
+      <div className="Footer">
+        Please edit src/components///FooterComponent.js to update this component!
+      </div>
+    );
+  }
+}
+
+FooterComponent.displayName = 'FooterComponent';
+
+// Uncomment properties you need
+// FooterComponent.propTypes = {};
+// FooterComponent.defaultProps = {};
+
+export default FooterComponent;
