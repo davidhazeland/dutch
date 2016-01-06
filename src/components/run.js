b046f8501e6deef3336ba271b7ee4310d8c06422
thanhcong <thanhcong.mssl@gmail.com>
2016-01-06 08:52:11 +0700

    Add redux router

--- src/components/run.js
+++ src/components/run.js
@@ -2,13 +2,27 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from '../stores';
-import App from '../containers/App';
+import { Router, Route, IndexRoute } from 'react-router'
+import { createHistory } from 'history'
+import { syncReduxAndRouter } from 'redux-simple-router'
 
+//import App from '../containers/App';
+import Main from '../components/Main';
+import PageReporter from '../containers/PageReporter';
+
+const history = createHistory();
 const store = configureStore();
+
+syncReduxAndRouter(history, store);
 
 render(
   <Provider store={store}>
-    <App />
+    <Router history={history}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={PageReporter} />
+        <Route path="page" component={PageReporter}/>
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('app')
 );
