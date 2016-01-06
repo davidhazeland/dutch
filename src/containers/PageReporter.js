13d84a3ac137907ff67b92d7f99d31701203fa10
thanhcong <thanhcong.mssl@gmail.com>
2016-01-06 08:53:55 +0700

    Add page reporter container

---
+++ src/containers/PageReporter.js
@@ -0,0 +1,34 @@
+import React, {
+  Component,
+  PropTypes
+} from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import PageReporterComponent from '../components/PageReporterComponent';
+
+class PageReporter extends Component {
+  render(){
+    const {actions, PageReporter} = this.props;
+    return <PageReporterComponent actions={actions} PageReporter={PageReporter}/>;
+  }
+}
+
+PageReporter.propTypes = {
+  actions: PropTypes.object.isRequired,
+  PageReporter: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state) {
+  /* Populated by react-webpack-redux:reducer */
+  const props = { PageReporter: state.PageReporter };
+  return props;
+}
+function mapDispatchToProps(dispatch) {
+  /* Populated by react-webpack-redux:action */
+  const actions = { Search: require('../actions/PageReporter/Search.js') };
+  const actionMap = { actions: bindActionCreators(actions, dispatch) };
+  return actionMap;
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PageReporter);
