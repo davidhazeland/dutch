1dc2bbffca935f1bbf9acf1456a56a90dd7d164f
thanhcong <thanhcong.mssl@gmail.com>
2016-01-06 08:41:13 +0700

    Apply redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import 'core-js/fn/object/assign';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/Main';
+//import React from 'react';
+//import ReactDOM from 'react-dom';
+//import App from './components/Main';
 
 // Render the main component into the dom
-ReactDOM.render(<App />, document.getElementById('app'));
+//ReactDOM.render(<App />, document.getElementById('app'));
+
+import './components/run';
