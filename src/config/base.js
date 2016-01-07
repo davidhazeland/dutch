4399924cd46ddb0aa663b75530d65eb39dd503e2
thanhcong <thanhcong.mssl@gmail.com>
2016-01-07 09:55:20 +0700

    Add google API config

--- src/config/base.js
+++ src/config/base.js
@@ -3,4 +3,8 @@
 
 // Settings configured here will be merged into the final config object.
 export default {
+  GOOGLE_API_CLIENT_ID: '60820199865-7tncjaor95g3sdhqe7gcefeobcp1vtn5.apps.googleusercontent.com',
+  GOOGLE_ANALYTICS_ACCOUNT_ID: '55012181',
+  GOOGLE_ANALYTICS_PROPERTY_ID: 'UA-55012181-3',
+  GOOGLE_ANALYTICS_VIEW_ID: '100697044'
 }
