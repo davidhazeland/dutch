c1b9af4f91a6464b4dd45585144f05a15fcef6b4
thanhcong <thanhcong.mssl@gmail.com>
2016-01-08 17:14:08 +0700

    Fix Google API

---
+++ src/gapi/AnalyticsCoreReporting.js
@@ -0,0 +1,53 @@
+/* global gapi */
+
+/**
+ * Query Google Analytics API
+ * @param profileId
+ * @param queryParams
+ * {
+ *  startDate,
+ *  endDate,
+ *  metrics: [
+ *
+ *  ],
+ *  dimensions: [
+ *
+ *  ],
+ *  sort: {
+ *    name,
+ *    direction
+ *  },
+ *  filter: [
+ *    {
+ *      name,
+ *      expression
+ *    }
+ *  ]
+ * }
+ * @returns Promise
+ */
+export function query(profileId, queryParams) {
+  const id = `ga:${profileId}`;
+  const startDate = queryParams.startDate;
+  const endDate = queryParams.endDate;
+  const metrics = queryParams.metrics.join(',');
+  const dimensions = queryParams.dimensions.join(',');
+  const sort = `${queryParams.sort.direction}${queryParams.sort.name}`;
+  const filters = queryParams.filters.map(filter => {
+    const operator = '=~'; // Regular Expression
+    return `${filter.name}${operator}${filter.expression}`
+  }).join(',');
+
+  return gapi.client.analytics.data.ga.get({
+    'ids': id,
+    'start-date': startDate,
+    'end-date': endDate,
+    'metrics': metrics,
+    'dimensions': dimensions,
+    'sort': sort,
+    'filters': filters,
+    'max-results': 10
+  });
+}
+
+
---
+++ src/gapi/PageTracking.js
@@ -0,0 +1,50 @@
+import Promise from 'bluebird';
+import config from 'config';
+
+import {query as queryAnalyticsReporting} from './AnalyticsCoreReporting';
+
+function mapParams(params) {
+  const sort = {
+    name: 'ga:pageviews',
+    direction: params.desc ? '-' : ''
+  };
+  const filters = [
+    {
+      name: 'ga:pagePath',
+      expression: params.keyword
+    }
+  ];
+
+  return {
+    startDate: params.startDate,
+    endDate: params.endDate,
+    sort: sort,
+    filters: filters
+  }
+}
+
+
+export function query(params) {
+  const pageMetrics = ['ga:pageviews', 'ga:uniquePageviews', 'ga:avgTimeOnPage', 'ga:entranceRate', 'ga:bounceRate', 'ga:exitRate', 'ga:pageValue'];
+  const pageDimensions = ['ga:pagePath'];
+
+  const paramsMapped = mapParams(params);
+
+  const analyticsReporting = queryAnalyticsReporting(config.GOOGLE_ANALYTICS_VIEW_ID, {
+    metrics: pageMetrics,
+    dimensions: pageDimensions,
+    startDate: paramsMapped.startDate,
+    endDate: paramsMapped.endDate,
+    sort: paramsMapped.sort,
+    filters: paramsMapped.filters
+  });
+
+  return new Promise((resolve, reject) => {
+    analyticsReporting.then(response => {
+        resolve(response.result);
+      })
+      .then(null, (err) => {
+        reject(err);
+      });
+  });
+}
