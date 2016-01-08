834d5942c441e497f823bc02f77e58ce6d5747a5
thanhcong <thanhcong.mssl@gmail.com>
2016-01-08 17:15:53 +0700

    Add page report reducer

---
+++ test/reducers/PageReporterTest.js
@@ -0,0 +1,12 @@
+var reducer = require('../../src/reducers/PageReporter');
+
+describe('PageReporter', () => {
+
+  it('should not change the passed state', (done) => {
+
+    const state = Object.freeze({});
+    reducer(state, {type: 'INVALID'});
+
+    done();
+  });
+});
