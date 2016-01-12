7aa40cda81983713897dd5a45c9872d0af5c8a17
thanhcong <thanhcong.mssl@gmail.com>
2016-01-12 21:07:23 +0700

    Add Overview reducer test

---
+++ src/actions/OverviewReceiveActiveUsers.js
@@ -0,0 +1,3 @@
+module.exports = function(parameter) {
+  return { type: 'OVERVIEW_RECEIVE_ACTIVE_USERS', parameter };
+};
--- src/reducers/Overview.js
+++ src/reducers/Overview.js
@@ -3,19 +3,36 @@
  * If you change the type from object to something else, do not forget to update
  * src/container/App.js accordingly.
  */
+import Immutable from 'immutable';
+
 const initialState = {};
+
+function requestActiveUsers(state) {
+  return state.setIn(['activeUser', 'isRequesting'], true);
+}
+
+function receiveActiveUsers(state, data) {
+  return state
+    .setIn(['activeUser', 'isRequesting'], false)
+    .setIn(['activeUser', 'data'], Immutable.fromJS(data));
+}
 
 module.exports = function(state = initialState, action) {
   /* Keep the reducer clean - do not mutate the original state. */
   //let nextState = Object.assign({}, state);
 
   switch(action.type) {
-    /*
-    case 'YOUR_ACTION': {
+
+    case 'OVERVIEW_REQUEST_ACTIVE_USERS': {
       // Modify next state depending on the action and return it
-      return nextState;
+      return requestActiveUsers(state);
     } break;
-    */
+
+    case 'OVERVIEW_RECEIVE_ACTIVE_USERS': {
+      // Modify next state depending on the action and return it
+      return receiveActiveUsers(state, action.parameter);
+    } break;
+
     default: {
       /* Return original state if no actions were consumed. */
       return state;
