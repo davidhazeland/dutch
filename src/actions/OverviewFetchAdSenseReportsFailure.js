9e9ed0c58caca8062bfd419dbf47e9560570aa53
thanhcong <thanhcong.mssl@gmail.com>
2016-01-18 16:50:21 +0700

    Add AdSense Report action

---
+++ src/actions/OverviewFetchAdSenseReportsFailure.js
@@ -0,0 +1,3 @@
+module.exports = function(parameter) {
+  return { type: 'OVERVIEW_FETCH_AD_SENSE_REPORTS_FAILURE', parameter };
+};
---
+++ src/actions/OverviewStopAdSenseReportsFailure.js
@@ -0,0 +1,3 @@
+module.exports = function(parameter) {
+  return { type: 'OVERVIEW_STOP_AD_SENSE_REPORTS_FAILURE', parameter };
+};
