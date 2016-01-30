7ae57c06fcba7ebd4c64d78cf6bb3762b8e69bce
thanhcong <thanhcong.mssl@gmail.com>
2016-01-30 17:08:07 +0700

    Use direct metrics and dimensions as parameter

--- src/gapi/RealTimeReporting.js
+++ src/gapi/RealTimeReporting.js
@@ -4,19 +4,11 @@ export function resolveId(id) {
   return `ga:${id}`;
 }
 
-export function resolveMetric(metrics) {
-  return metrics.join(',');
-}
-
-export function resolveDimension(dimensions) {
-  return dimensions.join(',');
-}
-
 export function query(viewId, {metrics, dimensions}) {
   const params = {
     ids: resolveId(viewId),
-    metrics: resolveMetric(metrics),
-    dimensions: resolveDimension(dimensions)
+    metrics: metrics,
+    dimensions: dimensions
   };
 
   return gapi.client.analytics.data.realtime.get(params);
