aecf96b82359c314456e7a1ccb5d2f197baaf8c5
thanhcong <thanhcong.mssl@gmail.com>
2016-01-31 15:20:25 +0700

    Refactoring Overview Fetch AdSense Report Request test

--- test/sagas/OverviewFetchAdSenseReportRequestTest.js
+++ test/sagas/OverviewFetchAdSenseReportRequestTest.js
@@ -13,7 +13,6 @@ import {fetch} from 'gapi/EarningReport.js';
 import OverviewFetchAdSenseReportsSuccess from 'actions/OverviewFetchAdSenseReportsSuccess';
 import OverviewFetchAdSenseReportsFailure from 'actions/OverviewFetchAdSenseReportsFailure';
 
-
 const setup = () => {
   const fixtures = {};
 
@@ -29,10 +28,6 @@ const setup = () => {
 
   fixtures.getState = () => fixtures.state;
 
-  fixtures.sagaIterator = saga(fixtures.getState);
-  fixtures.requestIterator = request(fixtures.getState);
-  fixtures.autoUpdateIterator = autoUpdate(fixtures.getState);
-
   return fixtures;
 };
 
@@ -40,149 +35,127 @@ const setup = () => {
 test('Overview Fetch AdSense Report Request saga', assert => {
   const fixtures = setup();
 
+  const sagaIterator = saga(fixtures.getState);
+
   const actual = [];
   const expected = [];
 
-  actual[0] = fixtures.sagaIterator.next().value;
+  actual[0] = sagaIterator.next().value;
   expected[0] = take('OVERVIEW_FETCH_ADSENSE_REPORTS_REQUEST');
 
-  assert.deepEqual(actual[0], expected[0],
-    'should wait for OVERVIEW_FETCH_ADSENSE_REPORTS_REQUEST action');
-
-  actual[1] = fixtures.sagaIterator.next(true).value;
+  actual[1] = sagaIterator.next(true).value;
   expected[1] = fork(request, fixtures.getState);
 
-  assert.deepEqual(actual[1], expected[1],
-    'should fork request when catch OVERVIEW_FETCH_ADSENSE_REPORTS_REQUEST action');
-
-  actual[2] = fixtures.sagaIterator.next().value;
+  actual[2] = sagaIterator.next().value;
   expected[2] = fork(autoUpdate, fixtures.getState);
 
-  assert.deepEqual(actual[2], expected[2],
-    'then fork auto update');
-
-  actual[3] = fixtures.sagaIterator.next().value;
+  actual[3] = sagaIterator.next().value;
   expected[3] = take('OVERVIEW_FETCH_ADSENSE_REPORTS_REQUEST');
 
-  assert.deepEqual(actual[3], expected[3],
-    'then continue waiting for OVERVIEW_FETCH_ADSENSE_REPORTS_REQUEST action');
+  assert.deepEqual(actual, expected,
+    'should request and auto update when catch request action');
 
   assert.end();
 });
 
 
 test('Overview Fetch AdSense Report Request saga: request generator', nest => {
-
   nest.test('...without error', assert => {
     const fixtures = setup();
+    const requestIterator = request(fixtures.getState);
 
     const actual = [];
     const expected = [];
 
-    actual[0] = fixtures.requestIterator.next().value;
+    actual[0] = requestIterator.next().value;
 
     const account = fixtures.state.Google.get('analyticsAccounts').get(0).toJS();
     expected[0] = call(fetch, account);
 
-    assert.deepEqual(actual[0], expected[0],
-      'should call fetch service');
-
     const result = {};
 
-    actual[1] = fixtures.requestIterator.next(result).value;
+    actual[1] = requestIterator.next(result).value;
     expected[1] = put(OverviewFetchAdSenseReportsSuccess(result));
 
-    assert.deepEqual(actual[1], expected[1],
-      'then dispatch OVERVIEW_FETCH_ADSENSE_REPORTS_SUCCESS action');
+    assert.deepEqual(actual, expected,
+      'should dispatch success action');
 
     assert.end();
   });
 
   nest.test('...with error', assert => {
     const fixtures = setup();
+    const requestIterator = request(fixtures.getState);
 
     const actual = [];
     const expected = [];
 
-    actual[0] = fixtures.requestIterator.next().value;
+    actual[0] = requestIterator.next().value;
 
     const account = fixtures.state.Google.get('analyticsAccounts').get(0).toJS();
     expected[0] = call(fetch, account);
 
-    assert.deepEqual(actual[0], expected[0],
-      'should call fetch service');
-
     const error = new Error();
 
-    actual[1] = fixtures.requestIterator.throw(error).value;
+    actual[1] = requestIterator.throw(error).value;
     expected[1] = put(OverviewFetchAdSenseReportsFailure(error));
 
-    assert.deepEqual(actual[1], expected[1],
-      'then dispatch OVERVIEW_FETCH_ADSENSE_REPORTS_FAILURE action');
+    assert.deepEqual(actual, expected,
+      'should dispatch failure action');
 
     assert.end();
   });
-
 });
 
 
 test('Overview Fetch AdSense Report Request saga: autoUpdate generator', nest => {
-
   nest.test('...request interval', assert => {
     const fixtures = setup();
+    const autoUpdateIterator = autoUpdate(fixtures.getState);
 
     const actual = [];
     const expected = [];
 
-    actual[0] = fixtures.autoUpdateIterator.next().value;
+    actual[0] = autoUpdateIterator.next().value;
     expected[0] = race({
       tick: call(delay, config.OVERVIEW_ADSENSE_REPORTS_REFRESH_PERIOD),
       stop: take('OVERVIEW_STOP_FETCH_ADSENSE_REPORTS')
     });
 
-    assert.deepEqual(actual[0], expected[0],
-      'should wait for OVERVIEW_STOP_FETCH_ADSENSE_REPORTS action or timeout');
-
-    actual[1] = fixtures.autoUpdateIterator.next({stop: false}).value;
+    actual[1] = autoUpdateIterator.next({stop: false}).value;
     expected[1] = fork(request, fixtures.getState);
 
-    assert.deepEqual(actual[1], expected[1],
-      'should fork request generator when timeout');
-
-    actual[2] = fixtures.autoUpdateIterator.next().value;
+    actual[2] = autoUpdateIterator.next().value;
     expected[2] = race({
       tick: call(delay, config.OVERVIEW_ADSENSE_REPORTS_REFRESH_PERIOD),
       stop: take('OVERVIEW_STOP_FETCH_ADSENSE_REPORTS')
     });
 
-    assert.deepEqual(actual[2], expected[2],
-      'then continue waiting for OVERVIEW_STOP_FETCH_ADSENSE_REPORTS action or timeout');
+    assert.deepEqual(actual, expected,
+      'should request when timeout');
 
     assert.end();
   });
 
   nest.test('..with stop action', assert => {
     const fixtures = setup();
+    const autoUpdateIterator = autoUpdate(fixtures.getState);
 
     const actual = [];
     const expected = [];
 
-    actual[0] = fixtures.autoUpdateIterator.next().value;
+    actual[0] = autoUpdateIterator.next().value;
     expected[0] = race({
       tick: call(delay, config.OVERVIEW_REFRESH_PERIOD),
       stop: take('OVERVIEW_STOP_FETCH_ADSENSE_REPORTS')
     });
 
-    assert.deepEqual(actual[0], expected[0],
-      'should wait for OVERVIEW_STOP_FETCH_ADSENSE_REPORTS action or timeout');
-
-    actual[1] = fixtures.autoUpdateIterator.next({stop: true}).value;
+    actual[1] = autoUpdateIterator.next({stop: true}).value;
     expected[1] = undefined;
 
-    assert.deepEqual(actual[1], expected[1],
-      'should exit when catch OVERVIEW_STOP_FETCH_ADSENSE_REPORTS action');
+    assert.deepEqual(actual, expected,
+      'should exit when catch stop action');
 
     assert.end();
   });
-
 });
