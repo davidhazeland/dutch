078f37aac722de4b5b6e810d524c90bc7c10cd4c
thanhcong <thanhcong.mssl@gmail.com>
2016-01-31 15:20:38 +0700

    Remove unused module

--- src/sagas/GoogleAuthorizeRequest.js
+++ src/sagas/GoogleAuthorizeRequest.js
@@ -1,6 +1,5 @@
 import {call, put, take, fork} from 'redux-saga';
 import {authorize as googleAuthorize} from '../gapi/OAuth';
-import GoogleAuthorizeRequest from '../actions/GoogleAuthorizeRequest';
 import GoogleAuthorizeSuccess from '../actions/GoogleAuthorizeSuccess';
 import GoogleAuthorizeFailure from '../actions/GoogleAuthorizeFailure';
 
