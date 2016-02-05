ed96850b1451bdf9d784177f138de9effafa00d2
thanhcong <thanhcong.mssl@gmail.com>
2016-02-05 10:13:50 +0700

    Add propTypes for Header component

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -2,42 +2,55 @@
 
 import cx from 'classnames';
 import { Link } from 'react-router';
+import ImmutablePropTypes from 'react-immutable-proptypes';
 
 require('styles/components//Header.less');
 
 import logo from '../images/logo.png';
 
-export default React => ({Google, Facebook, actions: {GoogleAuthorizeRequest}}) => {
-  const authGoogleClass = cx('Header-authButton ui item', {
-    'visible': !Google.get('authorized')
-  });
+export default React => {
+  const {shape, func} = React.PropTypes;
 
-  const authFacebookClass = cx('Header-authButton ui item', {
-    'visible': !Facebook.get('authorized')
-  });
+  const Header = ({Google, Facebook, actions: {GoogleAuthorizeRequest}}) => {
+    const authGoogleClass = cx('Header-authButton ui item', {
+      'visible': !Google.get('authorized')
+    });
 
-  const authGoogleText = Google.get('isLogining') ? 'Logining' : 'Authorize';
+    const authFacebookClass = cx('Header-authButton ui item', {
+      'visible': !Facebook.get('authorized')
+    });
 
-  return (
-    <div className="Header">
-      <div className="ui secondary pointing menu">
-        <div className="header item">
-          <img className="logo" src={logo} alt=""/>
-          Dutch Project
-        </div>
-        <Link to="/overview" className="item active blue">Home</Link>
-        <Link to="#" className="item">Reporting</Link>
-        <div className="right menu">
-          <button className={authGoogleClass}
-                  onClick={() => GoogleAuthorizeRequest}>
-            {authGoogleText} Google
-          </button>
-          <button className={authFacebookClass}
-                  onClick={() => {}}>
-            Login Facebook
-          </button>
+    const authGoogleText = Google.get('isLogining') ? 'Logining' : 'Authorize';
+
+    return (
+      <div className="Header">
+        <div className="ui secondary pointing menu">
+          <div className="header item">
+            <img className="logo" src={logo} alt=""/>
+            Dutch Project
+          </div>
+          <Link to="/overview" className="item active blue">Home</Link>
+          <Link to="#" className="item">Reporting</Link>
+          <div className="right menu">
+            <button className={authGoogleClass}
+                    onClick={() => GoogleAuthorizeRequest}>
+              {authGoogleText} Google
+            </button>
+            <button className={authFacebookClass}
+                    onClick={() => {}}>
+              Login Facebook
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  };
+
+  Header.propTypes = {
+    Google: ImmutablePropTypes.map.isRequired,
+    Facebook: ImmutablePropTypes.map.isRequired,
+    actions: shape({
+      GoogleAuthorizeRequest: func.isRequired
+    })
+  }
 };
